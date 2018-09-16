c4dca461b69fa2a259b17b63650709c00690c92e
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-09-16 18:24:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function dropdown() {
+	var x = document.getElementById("dropdown-content");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
