1bed3f98d8b399d94ae55b328a226f00d11d244b
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-09-16 18:36:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 alert("Potato");
 
 function dropdown() {
-	alert();
 	for (var i = 0; i < document.getElementsByClassName("dropdown-content").length; i++) {
+		alert(i);
 		var x = document.getElementsByClassName("dropdown-content")[i];
 		if (x.style.display === "none") { 
 			x.style.display = "block";
