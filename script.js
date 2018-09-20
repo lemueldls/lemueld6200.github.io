ac491ee2ad57708423f2ea0a29e958eb1f3a3b64
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-09-20 13:48:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var oof = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3")
 window.onclick = function() {
+  var oof = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
   oof.play();
   alert(oof);
 };
