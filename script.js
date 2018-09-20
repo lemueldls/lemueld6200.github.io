74f96fe401858de63be173934fd95267692c427a
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-09-20 12:39:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-function dropdown() {
-	var x = document.getElementsByClassName("dropdown-content");
-	for (var i = 0; i < x.length; i++) {
-		if (x[i].style.display === "none") { 
-			x[i].style.display = "block";
-		}
-		else {
-			x[i].style.display = "none";
-		}
-	}
-}
+var oof = new Auido("/Roblox-death-sound.mp3")
+window.onclick = oof.play()
