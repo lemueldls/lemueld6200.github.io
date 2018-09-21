b1d95e450d7e4ba4c0d1652c7219e159fe418eae
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-09-21 13:50:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-window.onclick = function() {
-  var oof = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
-  oof.play();
+window.ontouchstart = oof();
+function oof() {
+	setInterval(function() {
+		var oofAud = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
+		oofAud.play();
+	}, 1);
 };
 window.$crisp=[];
 window.CRISP_WEBSITE_ID = "77942a01-94cb-4616-8c2d-5318e51ac865";
