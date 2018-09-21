951b49fef5985eb682428d628feb765e5ce06ff2
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-09-21 17:27:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var oofLoop;
 
-window.onmousedown = function() { oofLoop = setInterval(function() { oof() }, 1) };
-window.onmouseup = function() { clearInterval(oofLoop) };
-window.ontouchstart = function() { oofLoop = setInterval(function() { oof() }, 1) };
-window.ontouchend = function() { clearInterval(oofLoop) };
+onclick = oof();
+
+/* v=!=v=!= !-v-!DANGER ZONE!-v-! =!=v=!=v
+ * window.onmousedown = function() { oofLoop = setInterval(function() { oof() }, 1) };
+ * window.onmouseup = function() { clearInterval(oofLoop) };
+ * window.ontouchstart = function() { oofLoop = setInterval(function() { oof() }, 1) };
+ * window.ontouchend = function() { clearInterval(oofLoop) };
+*/
+
 function oof() {
-	oofLoop = setInterval(function() {
-		var oofAud = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
-		oofAud.play();
-	}, 1);
+	var oofAud = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
+	oofAud.play();
 };
 window.$crisp=[];
 window.CRISP_WEBSITE_ID = "77942a01-94cb-4616-8c2d-5318e51ac865";
