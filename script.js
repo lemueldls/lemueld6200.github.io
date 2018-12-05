c0a1b482696fd45eec2a87d5a02f3d640462cf76
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-12-05 10:47:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+window.onload = function() {
+	// X-Domain Cookies
+	var xd_cookie = xDomainCookie( '//my.s3bucket.com' );
+	xd_cookie.get( 'cookie_name', function(cookie_val){
+		//cookie val will contain value of cookie as fetched from local val (if present) else from iframe (if set), else null
+		if(!cookie_val){
+			var new_val = get_what_value_should_be();
+			xd_cookie.set( 'cookie_name', new_val );
+		}
+	});
+};
 // OOF
 document.getElementById("potato").onclick = function() {
   var oof = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
@@ -14,13 +25,3 @@ window.CRISP_WEBSITE_ID = "77942a01-94cb-4616-8c2d-5318e51ac865";
 	s.async = 1;
 	d.getElementsByTagName("head")[0].appendChild(s);
 })();
-
-// X-Domain Cookies
-var xd_cookie = xDomainCookie( '//my.s3bucket.com' );
-xd_cookie.get( 'cookie_name', function(cookie_val){
-	//cookie val will contain value of cookie as fetched from local val (if present) else from iframe (if set), else null
-	if(!cookie_val){
-		var new_val = get_what_value_should_be();
-		xd_cookie.set( 'cookie_name', new_val );
-	}
-});
