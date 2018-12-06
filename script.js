29c8b9691929d80560460cb6282a15fbb63a7e4c
Lemuel De Los Santos <43242425+lemueld6200@users.noreply.github.com>
2018-12-06 10:45:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ window.onload = function() {
 		}
 	});
 };
-// OOF
-document.getElementById("potato").onclick = function() {
-  var oof = new Audio("https://lemueld6200.github.io/Roblox-death-sound.mp3");
-  oof.play();
-};
 
 // Crisp
 window.$crisp=[];
